7ad376f90154d109d79895cce3eeb8c96027eb93
Wenguang <wenguangzhang@caixin.com>
2025-11-11 15:35:00 +0800

    node templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,19 @@
 import type { NodeExecutor } from "@/features/executions/type";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky"
+import Handlebars from "handlebars"
+import fetch, { type RequestInit } from 'node-fetch';
+
+Handlebars.registerHelper("json", (ctx) => {
+    const jsonString = JSON.stringify(ctx, null, 2)
+    const safeString = new Handlebars.SafeString(jsonString)
+    return safeString
+})
 
 type HttpRequestData = {
-    variableName?: string
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    variableName: string
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string
 }
 
@@ -20,29 +28,37 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     }
     if (!data.variableName) {
         // TODO: Publish "error" state for http request
-        throw new NonRetriableError("Variable name not configured")
+        throw new NonRetriableError("HTTP Request node: Variable name not configured")
+    }
+    if (!data.method) {
+        // TODO: Publish "error" state for http request
+        throw new NonRetriableError("HTTP Request node: Method name not configured")
     }
 
     // const result = await step.fetch(data.endpoint)
 
     //  Wait for the http request to be completed
     const result = await step.run("http-request", async () => {
-        const method = data.method || "GET"
+        const method = data.method
+        const endpoint = Handlebars.compile(data.endpoint)(context)
 
-        const endpoint = data.endpoint!
-
-        const options: KyOptions = {
+        const options: RequestInit = {
             method,
         }
 
         if (["POST", "PUT", "PATCH"].includes(method)) {
-            options.body = data.body;
+            const resolved = Handlebars.compile(data.body || "{}")(context)
+            JSON.parse(resolved)
+
+            options.body = resolved;
             options.headers = {
-                "Content-type": "application/json"
+                "Content-Type": "application/json"
             }
         }
 
-        const response = await ky(endpoint, options);
+        // TODO: something wrong with ky post. just don't get response. use node-fetch instaed ky for now.
+        const response = await fetch(endpoint, options);
+
         const contentType = response.headers.get("content-type")
 
         const responseData = contentType?.includes("application/json") ? await response.json() : await response.text()
@@ -56,11 +72,11 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
             }
         }
 
-        
+
 
         return {
             ...context,
-            [data.variableName!]: reponsePayload
+            [data.variableName]: reponsePayload
         }
     })
 
