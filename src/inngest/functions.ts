ccc20b7ea1cbe38cebd28e5d297a53537754ce87
Wenguang <wenguangzhang@caixin.com>
2025-11-20 16:02:53 +0800

    deploment

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -19,7 +19,7 @@ import { ExecutionStatus } from "@/generated/prisma";
 export const executeWorkflow = inngest.createFunction(
   {
     id: "execute-workflow",
-    retries: 0,
+    retries: process.env.NODE_ENV === "production" ? 3 : 0,
     onFailure: async ({ event, step }) => {
       return prisma.execution.update({
         where: {
